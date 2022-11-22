55d883c26e38f96fb70e0d5f159e28e06d7a837d
Darzanos <ntarzanos@aueb.gr>
2022-11-22 17:29:25 +0200

    create_topology.py generate_service_requests.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # This is a sample Python script.
 import numpy as np
 from create_topology import create_topology
+from generate_service_requests import generate_service_requests
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -28,28 +29,37 @@ if __name__ == '__main__':
     # Resource types based on our example
     # IoT Core, Kinesis Firehose, Kinesis Data Analytics, S3, EMR, QuickSight
     resource_types = {'IoT', 'Firehose', 'Analytics', 'S3', 'EMR','Quick'}
+    ResProf_prob = 0.35 # large profit with probability
 
     # Resource Capacities are assigned based on a normal distribution
-    resource_profile = dict()
+    resource_profile_small = dict()
+    resource_profile_large = dict()
     for r_type in resource_types:
         if r_type == 'IoT':
             # number of IoT Devices can be supported by a single providers in a single location
-            resource_profile[r_type] = 10000
+            resource_profile_small[r_type] = 10000
+            resource_profile_large[r_type] = 50000
         elif r_type == 'Firehose':
             # maximum number of TBs that can be streamed into Firehose resources
-            resource_profile[r_type] = 200
+            resource_profile_small[r_type] = 200
+            resource_profile_large[r_type] = 1000
         elif r_type == 'Analytics':
             # number of Data Analytics processing units available by each Provider in each region
-            resource_profile[r_type] = 100
+            resource_profile_small[r_type] = 100
+            resource_profile_large[r_type] = 500
         elif r_type == 'S3':
             # maximum number of TBs can be stored by each Provider in each region
-            resource_profile[r_type] = 500
+            resource_profile_small[r_type] = 500
+            resource_profile_large[r_type] = 2500
         elif r_type == 'EMR':
             # number of vCPUs maintained by a single providers in a single location
-            resource_profile[r_type] = 1000
+            resource_profile_small[r_type] = 1000
+            resource_profile_large[r_type] = 5000
         elif r_type == 'Quick':
             # number of Qyicksight instances can be supported by a single providers in a single location
-            resource_profile[r_type] = 10**6  # THIS IN FACT MEANS NO LIMITATION
+            resource_profile_small[r_type] = 10**6  # THIS IN FACT MEANS NO LIMITATION
+            resource_profile_large[r_type] = 10**6
+
 
     # ##### Service Characteristics
     # mean default load of service requests # we consider the AWS connected mobility service presented in the example
@@ -67,7 +77,12 @@ if __name__ == '__main__':
     # Load_Core[1] = [100, 200, 1]
     # Load_Core[1] = [200, 400, 1]
 
+    # service price base
     price_s = 100 # $/hour
+
+    # probability of addition service region
+    prob_region = 0.2
+
     ###############################
     #       Init Process          #
     ##############################
@@ -77,9 +92,9 @@ if __name__ == '__main__':
         for L in Loc:
             for top in range(1,random_topologies):
                 # create topology
-                T = create_topology(I, L, Loc_prob, resource_profile, resource_types)
+                T = create_topology(I, L, Loc_prob, ResProf_prob, resource_profile_small, resource_profile_large, resource_types)
 
                 # Generate Requests for different global loads
                 req = dict()
                 for S in SS:
-                    req[S] = generate_service_requests(S, L,Load_Core,Load_Edge, price_s)
+                    req[S] = generate_service_requests(S, L, Load_Core, Load_Edge, price_s, prob_region)
